6e411e95bc4975176bf6d5db9949ca3430c4925a
msqqcq <ms873248332@163.com>
2023-01-16 16:31:55 +0800

    init

---
+++ app/src/main/java/com/example/running/B.java
@@ -0,0 +1,13 @@
+package com.example.running;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class B extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helloworld);
+    }
+}
